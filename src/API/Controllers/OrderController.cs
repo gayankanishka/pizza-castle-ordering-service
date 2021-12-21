50173a5c369362c1c1b069f0daa937e0f332424a
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2021-12-21 06:52:51 +0530

    feat: add api changes

--- src/API/Controllers/OrderController.cs
+++ src/API/Controllers/OrderController.cs
@@ -2,29 +2,47 @@ using System.Net;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using PizzaCastle.OrderingService.Application.Orders.Commands.CheckoutOrder;
+using PizzaCastle.OrderingService.Application.Orders.Queries.GetOrdersByBuyerId;
 using PizzaCastle.OrderingService.Domain.Dtos;
 
 namespace PizzaCastle.OrderingService.API.Controllers
 {
-    [Route("api/v1/orders")]
+    [Route("api/v1/order")]
     [ApiController]
-    public class OrdersController : ControllerBase
+    public class OrderController : ControllerBase
     {
         private readonly IMediator _mediator;
         
-        public OrdersController(IMediator mediator)
+        public OrderController(IMediator mediator)
         {
             _mediator = mediator;
         }
         
         [HttpPost]
         [Route("checkout")]
-        [ProducesResponseType(typeof(CheckoutOrderDto), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(CheckoutOrderDto), (int)HttpStatusCode.Accepted)]
         [ProducesResponseType((int)HttpStatusCode.BadRequest)]
-        public async Task<IActionResult> CheckoutOrderAsync([FromBody] CheckoutOrderCommand command)
+        public async Task<IActionResult> CheckoutOrderAsync([FromBody] CheckoutOrderCommand command,
+            CancellationToken cancellationToken)
         {
-            var result = await _mediator.Send(command);
-            return Accepted(result);
+            var result = await _mediator.Send(command, cancellationToken);
+
+            if (result.Accepted)
+            {
+                return Accepted(result);
+            }
+
+            return BadRequest();
+        }
+        
+        [HttpGet]
+        [Route("{buyerId}/history")]
+        [ProducesResponseType(typeof(IEnumerable<OrderDto>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> CheckoutOrderAsync([FromRoute] string buyerId,
+            CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(new GetOrdersByBuyerIdQuery(buyerId), cancellationToken);
+            return Ok(result);
         }
     }
 }
